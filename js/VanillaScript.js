2b8c74007535274748af2fc2d33f40e4e7b03f13
Silent_Coder <36187018+Silentxknight@users.noreply.github.com>
2020-01-11 01:07:40 +0530

    Update VanillaScript.js

--- js/VanillaScript.js
+++ js/VanillaScript.js
@@ -1,39 +1,39 @@
 // ?code by Silent_Coder AKA SilentxKnight 
 // !vanila way
-var xhr= new XMLHttpRequest();
+// var xhr= new XMLHttpRequest();
 
-var nameBtn=document.getElementById("nameg");
-var jobBtn=document.getElementById("jobg")
+// var nameBtn=document.getElementById("nameg");
+// var jobBtn=document.getElementById("jobg")
 
-nameBtn.addEventListener('click',loadName);
-jobBtn.addEventListener('click',loadJobname);
+// nameBtn.addEventListener('click',loadName);
+// jobBtn.addEventListener('click',loadJobname);
 
-function loadName() {
-   xhr.open("GET", "http://www.filltext.com?rows=1&f={firstName}", true);
-   xhr.onload=function() {
-  if (this.readyState == 4 && this.status == 200) {
-   var outPut=JSON.parse(xhr.responseText);
-   document.getElementById('outputs').innerHTML="<h4> Ranadom Name Is: "+outPut[0].f+"</h4>";
-   // console.log(outPut);
+// function loadName() {
+//    xhr.open("GET", "http://www.filltext.com?rows=1&f={firstName}", true);
+//    xhr.onload=function() {
+//   if (this.readyState == 4 && this.status == 200) {
+//    var outPut=JSON.parse(xhr.responseText);
+//    document.getElementById('outputs').innerHTML="<h4> Ranadom Name Is: "+outPut[0].f+"</h4>";
+//    // console.log(outPut);
 
- }
-}
-  xhr.send();   
-}
+//  }
+// }
+//   xhr.send();   
+// }
 
-function loadJobname() {
-   xhr.open("GET", "http://www.filltext.com?rows=1&job={business}", true);
-   xhr.onload=function() {
-  if (this.readyState == 4 && this.status == 200) {
-   var outPut=JSON.parse(xhr.responseText);
-   document.getElementById('outputs2').innerHTML="<h4> Ranadom Name Is: "+outPut[0].job+"</h4>";
-   // console.log(outPut);
+// function loadJobname() {
+//    xhr.open("GET", "http://www.filltext.com?rows=1&job={business}", true);
+//    xhr.onload=function() {
+//   if (this.readyState == 4 && this.status == 200) {
+//    var outPut=JSON.parse(xhr.responseText);
+//    document.getElementById('outputs2').innerHTML="<h4> Ranadom Name Is: "+outPut[0].job+"</h4>";
+//    // console.log(outPut);
 
- }
-}
-  xhr.send();
+//  }
+// }
+//   xhr.send();
    
-}
+// }
 
 
 
